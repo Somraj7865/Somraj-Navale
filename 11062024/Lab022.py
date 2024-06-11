63230356dc32f7b10f921ea827929d6960d4c128
Somraj7865 <somrajnavale@gmail.com>
2024-06-11 23:26:19 +0530

    Literals and the operators

---
+++ 11062024/Lab022.py
@@ -0,0 +1,9 @@
+#Take two numbers and calculate first number is greater than less than or equals to second number
+Num1=int(input("Enter First Number: "))
+Num2=int(input("Enter Second Number: "))
+
+if Num1>Num2:
+    print("First number is greater than Second number")
+else:
+    if Num1<Num2: print("Second number is greater than First number")
+if Num1==Num2:print("Both the numbers are equals")
